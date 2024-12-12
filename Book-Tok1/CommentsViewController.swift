787793526e09d2ae8af2ee403bc7bfe3645f7bf8
monyshchenko01 <m.onyshchenko@kse.org.ua>
2024-12-12 09:43:00 +0200

    ViewController for adding comments

--- Book-Tok1/CommentsViewController.swift
+++ Book-Tok1/CommentsViewController.swift
@@ -1,72 +1,70 @@
-//
-//  CommentsViewController.swift
-//  Book-Tok1
-//
-//  Created by Matvii Onyshchenko on 06.12.2024.
-//
-
 import UIKit
 import SnapKit
 
-class CommentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class CommentsViewController: UIViewController, UITableViewDataSource {
     var comments: [String] = []
-    private lazy var tableView: UITableView = {
+
+    private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CommentCell")
-        tableView.dataSource = self
-        tableView.delegate = self
         tableView.separatorStyle = .none
         tableView.backgroundColor = .systemGray6
         return tableView
     }()
 
-    private var commentTextField: UITextField = {
+    private let commentTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Write a comment..."
         textField.borderStyle = .roundedRect
-        textField.backgroundColor = .white
         textField.layer.cornerRadius = 8
         textField.layer.borderWidth = 1
         textField.layer.borderColor = UIColor.lightGray.cgColor
         return textField
     }()
-    private var submitButton: UIButton = {
+
+    private let submitButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Add Comment", for: .normal)
         button.backgroundColor = .systemBlue
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 10
-        button.addTarget(CommentsViewController.self, action: #selector(addComment), for: .touchUpInside)
         return button
     }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        tableView.dataSource = self
+        submitButton.addTarget(self, action: #selector(addComment), for: .touchUpInside)
+        setupView()
+    }
+
+    private func setupView() {
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.left.right.equalToSuperview()
             $0.bottom.equalToSuperview().offset(-120)
         }
+
         let inputContainer = UIView()
-        inputContainer.backgroundColor = .white
-        inputContainer.layer.shadowColor = UIColor.black.cgColor
-        inputContainer.layer.shadowOpacity = 0.1
-        inputContainer.layer.shadowOffset = CGSize(width: 0, height: -2)
-        inputContainer.layer.shadowRadius = 4
         view.addSubview(inputContainer)
+
         inputContainer.snp.makeConstraints {
             $0.left.right.bottom.equalToSuperview()
             $0.height.equalTo(120)
         }
+
         inputContainer.addSubview(commentTextField)
         inputContainer.addSubview(submitButton)
+
         commentTextField.snp.makeConstraints {
             $0.left.equalToSuperview().offset(16)
             $0.right.equalTo(submitButton.snp.left).offset(-16)
             $0.top.equalToSuperview().offset(20)
             $0.height.equalTo(40)
         }
+
         submitButton.snp.makeConstraints {
             $0.right.equalToSuperview().offset(-16)
             $0.top.equalToSuperview().offset(20)
@@ -74,9 +72,11 @@ class CommentsViewController: UIViewController, UITableViewDataSource, UITableVi
             $0.height.equalTo(40)
         }
     }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return comments.count
     }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath)
         cell.textLabel?.text = comments[indexPath.row]
@@ -85,17 +85,9 @@ class CommentsViewController: UIViewController, UITableViewDataSource, UITableVi
         cell.backgroundColor = .white
         cell.layer.cornerRadius = 10
         cell.clipsToBounds = true
-        let containerView = UIView()
-        containerView.backgroundColor = .clear
-        containerView.layer.cornerRadius = 10
-        containerView.clipsToBounds = false
-        containerView.layer.shadowColor = UIColor.black.cgColor
-        containerView.layer.shadowOpacity = 0.1
-        containerView.layer.shadowOffset = CGSize(width: 0, height: 2)
-        containerView.layer.shadowRadius = 4
-        containerView.addSubview(cell)
         return cell
     }
+
     @objc private func addComment() {
         guard let text = commentTextField.text, !text.isEmpty else { return }
         comments.append(text)
