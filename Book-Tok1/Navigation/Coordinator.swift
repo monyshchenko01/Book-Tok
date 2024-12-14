96ea92a61588963588efa4bb2bebbad711d52e32
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-12-14 18:31:25 +0200

    coordinator

--- Book-Tok1/Navigation/Coordinator.swift
+++ Book-Tok1/Navigation/Coordinator.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class Coordinator {
     let navigationController: UINavigationController
+    let window: UIWindow
+    private let bookAPIService: BookAPIService
 
-    init(rootViewController: UINavigationController) {
-        self.navigationController = rootViewController
+    init(navigationController: UINavigationController, window: UIWindow, APIservice: BookAPIService) {
+        self.navigationController = navigationController
+        self.window = window
+        self.bookAPIService = APIservice
     }
+    
     func start() {
+        let tabBarController = UITabBarController()
+        
+        let bookTokViewModel = BookTokViewModel(bookAPIservice: bookAPIService)
+        let bookTokViewController = BookTokViewController(viewModel: bookTokViewModel)
+        bookTokViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "house.fill"), tag: 0)
+        
         let commentsViewController = CommentsViewController()
-        navigationController.viewControllers = [// тут буде перший контролер]
+        
+        tabBarController.viewControllers = [bookTokViewController]
+        navigationController.pushViewController(tabBarController, animated: false)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
     }
 
 }
--- Book-Tok1/SceneDelegate.swift
+++ Book-Tok1/SceneDelegate.swift
@@ -8,19 +8,16 @@
 import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
     var coordinator: Coordinator?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let navigationController = UINavigationController()
-        coordinator = Coordinator(rootViewController: navigationController)
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = navigationController
-        window?.backgroundColor = .systemGray2
-        window?.makeKeyAndVisible()
+        
+        let navigationContoller = UINavigationController()
+        let window = UIWindow(windowScene: windowScene)
+        let bookAPIService = BookAPIService()
+        
+        coordinator = Coordinator(navigationController: navigationContoller, window: window, APIservice: bookAPIService)
         coordinator?.start()
     }
 
