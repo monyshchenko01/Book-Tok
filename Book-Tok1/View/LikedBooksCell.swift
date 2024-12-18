09fd6f993d76ebdb584d285dea41930604025a55
monyshchenko01 <155827088+monyshchenko01@users.noreply.github.com>
2024-12-18 13:58:35 +0200

    Update LikedBooksCell.swift

--- Book-Tok1/View/LikedBooksCell.swift
+++ Book-Tok1/View/LikedBooksCell.swift
@@ -12,20 +12,16 @@ class BookCell: UITableViewCell {
         setupViews()
         setupGradientBackground()
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     private func setupViews() {
         titleLabel.font = .boldSystemFont(ofSize: 16)
         authorLabel.font = .systemFont(ofSize: 14)
         authorLabel.textColor = .gray
-        
         contentView.addSubview(bookImageView)
         contentView.addSubview(titleLabel)
         contentView.addSubview(authorLabel)
-        
         bookImageView.contentMode = .scaleAspectFill
         bookImageView.layer.cornerRadius = 16
         bookImageView.clipsToBounds = true
@@ -34,13 +30,11 @@ class BookCell: UITableViewCell {
             $0.width.equalTo(100)
             $0.height.equalTo(140)
         }
-        
         titleLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(8)
             $0.leading.equalTo(bookImageView.snp.trailing).offset(8)
             $0.trailing.equalToSuperview().inset(8)
         }
-        
         authorLabel.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(4)
             $0.leading.equalTo(bookImageView.snp.trailing).offset(8)
@@ -48,25 +42,18 @@ class BookCell: UITableViewCell {
             $0.bottom.equalToSuperview().inset(8)
         }
     }
-    
     private func setupGradientBackground() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = contentView.bounds
-        gradientLayer.colors = [
-            UIColor.lightGray.cgColor,
-            UIColor.darkGray.cgColor
-        ]
+        gradientLayer.colors = [UIColor.lightGray.cgColor, UIColor.darkGray.cgColor]
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-        
         contentView.layer.insertSublayer(gradientLayer, at: 0)
     }
-
 
     func configure(with book: Book) {
         titleLabel.text = book.title
         authorLabel.text = book.authors?.joined(separator: ", ")
-        
         if let urlString = book.imageLinks?.thumbnail, let url = URL(string: urlString) {
             loadImage(from: url)
         } else if let urlString = book.imageLinks?.smallThumbnail, let url = URL(string: urlString) {
